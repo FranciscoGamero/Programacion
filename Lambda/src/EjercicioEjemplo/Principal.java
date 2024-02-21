eaf6de9bdb59727d7f42c824ff935b9382a62f38
Francisco Manuel Gamero Rodríguez <gamero.rofra24@triana.salesianos.edu>
2024-02-21 11:31:58 +0100

    Lamda

---
+++ Lambda/src/EjercicioEjemplo/Principal.java
@@ -0,0 +1,20 @@
+package EjercicioEjemplo;
+
+public class Principal {
+
+	/* Como ejemplo para utilizar las lambdas vamos a utilizar la gestion de una discoteca,
+	 * donde hay una clase "Portero" que se caracteriza por su nombre, dni, sueldo y una lista
+	 * de clientes vips.
+	 * Hay una clase "Cliente", que se caracteriza por su nombre, dni, edad y las botellas que ha
+	 * pedido durante la noche. Esta tiene una clase hija "ClienteVip", que dispone de un cierto
+	 * numero de invitaciones para acompañantes.
+	 * 
+	 * El portero dejará entrar solamente aquellos que sean mayores de edad y a los vips
+	 * 
+	 * Los cliente tienen un método para comprar botellas, las cuales tendrán un precio fijo 
+	 * */
+	public static void main(String[] args) {
+
+	}
+
+}
