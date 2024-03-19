a8df5850859382eba087a6288d8ed08416f136cc
Francisco Manuel <fgamerorodriguez2905@gmail.com>
2024-03-19 15:48:34 +0100

    Todo

--- EjerciciosFechaYDerivados/src/ejercicio02/Principal.java
+++ EjerciciosFechaYDerivados/src/ejercicio02/Principal.java
@@ -18,15 +18,36 @@ public class Principal {
 		lista.add(new Palabra("Zamora"));
 		
 		ListaPalabras ges = new ListaPalabras(lista);
-		/*ges.mostrarLista(ges.getLista());
-		System.out.println("\n\n\n  ");
-		ges.mostrarLista(ges.ordenarAlfabeticamente());*/
+		ges.mostrarLista(ges.getLista());
 		
-		/*
+		System.out.println("ORDENAR ALFABETICAMENTE ORDENAR ALFABETICAMENTE ORDENAR ALFABETICAMENTE ORDENAR ALFABETICAMENTE");
+		ges.mostrarLista(ges.ordenarAlfabeticamente());
+		
+		System.out.println("ORDENAR ALFABETICAMENTE ORDENAR ALFABETICAMENTE ORDENAR ALFABETICAMENTE ORDENAR ALFABETICAMENTE\n\n");
+
+		System.out.println("BUSCAR UNA PALABRA BUSCAR UNA PALABRA BUSCAR UNA PALABRA BUSCAR UNA PALABRA BUSCAR UNA PALABRA");
 		ges.mostrarLista(ges.buscarPalabra("Coche"));
 		ges.mostrarLista(ges.buscarPalabra("sñlkdjklj"));
-		*/
-		System.out.println(ges.primerLetra("Coche"));
 		
+		System.out.println("BUSCAR UNA PALABRA BUSCAR UNA PALABRA BUSCAR UNA PALABRA BUSCAR UNA PALABRA BUSCAR UNA PALABRA\n\n");
+
+		System.out.println("CREAR PALABRA CREAR PALABRA CREAR PALABRA CREAR PALABRA CREAR PALABRA CREAR PALABRA CREAR PALABRA ");
+		lista.add(new Palabra(ges.crearPalabra()));
+		ges.mostrarLista(ges.getLista());
+
+		System.out.println("CREAR PALABRA CREAR PALABRA CREAR PALABRA CREAR PALABRA CREAR PALABRA CREAR PALABRA CREAR PALABRA\n\n");
+		
+		System.out.println("ELIMINAR IMPARES ELIMINAR IMPARES ELIMINAR IMPARES ELIMINAR IMPARES ELIMINAR IMPARES ELIMINAR IMPARES ");
+		ges.borrarImpares();
+		ges.mostrarLista(ges.getLista());
+	
+		System.out.println("ELIMINAR IMPARES ELIMINAR IMPARES ELIMINAR IMPARES ELIMINAR IMPARES ELIMINAR IMPARES ELIMINAR IMPARES\n\n");
+		
+		System.out.println("PASAR A MAYUSCULAS PASAR A MAYUSCULAS PASAR A MAYUSCULAS PASAR A MAYUSCULAS PASAR A MAYUSCULAS PASAR A MAYUSCULAS");
+		
+		ges.pasarAMayusculas();
+		ges.mostrarLista(ges.getLista());
+		
+		System.out.println("PASAR A MAYUSCULAS PASAR A MAYUSCULAS PASAR A MAYUSCULAS PASAR A MAYUSCULAS PASAR A MAYUSCULAS PASAR A MAYUSCULAS\n\n");
 		}
 }
